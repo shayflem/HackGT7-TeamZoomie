e977379e1db23e949f949b476b8c4c8c8918e295
Shaylah Fleming <68482745+shayflem@users.noreply.github.com>
2020-10-17 14:23:06 -0400

    Update Discordbot.py

--- Discordbot.py
+++ Discordbot.py
@@ -15,10 +15,17 @@ async def on_member_join(member):
 async def on_member_remove(member):
     print(f"{member} has left the server.")
 
+@client.command()
+async def ping(ctx):
+    await ctx.send(f"Pong! {round(client.latency * 1000)}ms")
 
 
 
-client.run("NzY2OTExNzE2NTI4NDg4NDY4.X4qQMQ.rVG9vHQsskNeT8JeVqpIZZFFrJI")
+
+
+
+
+client.run("NzY2OTExNzE2NTI4NDg4NDY4.X4qQMQ._tVC7A7jBTLFtH45LEeTBscMDcQ")
 
 
 
