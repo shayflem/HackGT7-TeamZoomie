a374965e82c4960164d098d09bac6d70de4094f1
Shaylah Fleming <68482745+shayflem@users.noreply.github.com>
2020-10-17 12:37:26 -0400

    Discordbot.py
    .

--- Discordbot.py
+++ Discordbot.py
@@ -1,12 +1,24 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix=".")
+
+@client.event
+async def on_ready():
+    print("Bot is ready.")
+
+@client.event
+async def on_member_join(member):
+    print(f"{member} has joined the server.")
+
+@client.event
+async def on_member_remove(member):
+    print(f"{member} has left the server.")
 
 
 
 
-
-
-
-
-
+client.run("NzY2OTExNzE2NTI4NDg4NDY4.X4qQMQ.rVG9vHQsskNeT8JeVqpIZZFFrJI")
 
 
 
