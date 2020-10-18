aea0470086cd119f402d872969a14e25128e234e
Shaylah Fleming <68482745+shayflem@users.noreply.github.com>
2020-10-17 20:30:48 -0400

    Create CareList.py

---
+++ CareList.py
@@ -0,0 +1,6 @@
+List = ["Be Kind to Yourself","Zoomie Loves You =)", "Life Will Get Better","Make Sure You Eat Today"]
+print("\nAffirmation List: ") #Make this this the tips list
+print(List[0])  
+print(List[1]) 
+print(List[2])  
+print(List[3]) 
