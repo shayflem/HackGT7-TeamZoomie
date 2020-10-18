849a4daf660f72d0adcb1b78a7cd383b78c19b67
Shaylah Fleming <68482745+shayflem@users.noreply.github.com>
2020-10-17 20:32:37 -0400

    Update CareList.py

--- CareList.py
+++ CareList.py
@@ -1,6 +1,10 @@
-List = ["Be Kind to Yourself","Zoomie Loves You =)", "Life Will Get Better","Make Sure You Eat Today"]
-print("\nAffirmation List: ") #Make this this the tips list
+List = ["Be Kind to Yourself","Zoomie Loves You =)", "Life Will Get Better","Make Sure You Eat Today"] #Modify with the tips
+print("\nSelf Care Tips : ") #Make this this the tips list
 print(List[0])  
 print(List[1]) 
 print(List[2])  
-print(List[3]) 
+print(List[3])  
+print(List[])  
+print(List[]) 
+print(List[])  
+print(List[]) 
